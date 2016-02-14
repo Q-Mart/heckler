a318a9e9fa00579dc0bb6f2d70c9a7c2cf586dc7
Q-Mart <qumarthjash@gmail.com>
2016-02-14 01:16:57 +0000

    JSON and iterator script

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var data = {
+	1: {
+		text: 'test 1',
+		audio: 'audio/1'
+	},
+
+	2: {
+		text: 'test 2',
+		audio: 'audio/2'
+	}
+};
+
+var dataLength = Object.keys(data).length;
+
+var objectPointer = 1;
+
 function heckle(){
-  console.log('Hello world');
+    if (objectPointer <= dataLength) {
+        currentObject = data[objectPointer];
+
+        $('#description').text(currentObject.text);
+        objectPointer++;
+    } else {
+        $('#description').text("Finished!");
+    }
+
 }
